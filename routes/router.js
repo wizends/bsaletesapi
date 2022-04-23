b0e5dcb71880da4626531ed524f5b842e910df69
wizends <ri.perezs@duocuc.cl>
2022-04-22 22:24:40 -0400

    comentarios

--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,16 @@
-const {Router} = require('express');
-const router = Router();
+/**Llamamos a todos los modulos necesarios, en este caso utilizare express y el operacional de Sequelize */
+
+const {Router} = require('express');//destructuring al nodo de express
+const router = Router();// instanciamos el router
+//traemos los modelos
 const Product = require('../models/products')
 const Category = require('../models/category')
 const Sequelize  = require('sequelize');
 const Op = Sequelize.Op;
 
-router.get('/pageQuantity', async (req,res) => {
+
+//****Aqui estaran todos los endpoints*********////////////
+router.get('/pageQuantity', async (req,res) => {//endpoint que consulta a la base de datos la cantidad de productos existentes
     const quantity = await Product.findAndCountAll({
         limit:0,
         offset:0
@@ -13,37 +18,37 @@ router.get('/pageQuantity', async (req,res) => {
     res.json(quantity.count)
 })
 
-router.get('/categories', async (req,res) => {
+router.get('/categories', async (req,res) => {//obtiene todas las categorias 
     try {
-        const categories2 = await Category.findAll();
-        res.status(200).json(categories2)
-    } catch (error) {
-        
+        const categories2 = await Category.findAll();//utilizamos findall de Sequelize, esto genera una consulta select dentro de la bd y obtiene todas las categorias
+        res.status(200).json(categories2)//enviamos la respuesta mediante el parametro res, ademas esperamos un status de 200
+    } catch (error) {//capturamos el error
+        console.log(error)//lo imprimimos en la consola
     }
 });
-router.get('/products', async (req, res) => {
+router.get('/products', async (req, res) => {//obtiene todos los productos sin paginacion
     const allProducts = await Product.findAll();
     res.json(allProducts)
 });
-router.get('/products/page', async (req,res)=>{
-    const {page,limit} = req.query;
-    const products = await Product.findAndCountAll({
-        limit:parseInt(limit),
-        offset:parseInt(page * limit)
+router.get('/products/page', async (req,res)=>{//obtiene los todos los productos paginados
+    const {page,limit} = req.query;//hacemos destructuring a la query recibida por la url donde viene page y limit
+    const products = await Product.findAndCountAll({//utilizamos findandcountall lo que traerla una respuesta mas elaborada, la cual servira para dividir la query dependiendo los parametros que le pasemos 
+        limit:parseInt(limit),//le mandamos el limit obtenido de la url
+        offset:parseInt(page * limit)// y mandamos el offset, el cual ira de un numero a otro, esto quiere decir que si le pasamos un 1 mostrara los productos que le indiquesmos y al cambiar de offset a 2 mostrara el limit + 1, por este mostivo se multiplica para obtener el resultado paginado
     });
-    res.json([products])
+    res.json([products])// se devuelve la respuesta en este caso un array que contiene nuestra response
 
 });
-router.get(`/categories/:id`, async (req,res) => {
+router.get(`/categories/:id`, async (req,res) => {//obtiene productos agrupados por categorias 
     
     try {
-        const id = req.params.id;
+        const id = req.params.id;//aqui utilizamos el bindeo mediante parametros enviados directamente por la url y los guardamos en una variable id 
         const productsPerCat2 = await Product.findAll({
             where:{category:id},
-        });
+        });// traemos todos los productos cuando su category sea igual al id recibido en los params
         res.json(productsPerCat2)
     } catch (error) {
-        
+        console.log(error)
     }
     
 });
--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-
+//llamamos las librerias necesarias 
 const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const app = express();
+const morgan = require('morgan');//morgan por alguna caso de excalabilidad
+const cors = require('cors');//cors para el tema de la cors policy 
+const app = express();// para utilizar express
 
-const port = 3004;
+const port = 3004;//definimos el puerto que queremos utilizar 
 
-app.use(express.json({
+app.use(express.json({//utilizamos express y fijamos que los archivos json no superen el mb por achivo 
     limit: '1mb'
 }
 ));
 
-const router = require('../routes/router')
+const router = require('../routes/router')//traemos nuestro router 
 
 
 /**Configuraciones */
-app.use(cors());
+app.use(cors());//instanciamos el cors a modo global 
 
-const port_number = app.listen(process.env.PORT || port);
-app.listen(port_number);
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+const port_number = app.listen(process.env.PORT || port);// utilizamos el puerto antes seleccionado en el caso de local host, pero si se aloja en un server el proccess.env.port se encargara de escoger el que se asigne, en caso de heroku es dinamico por lo que iria variando
+app.listen(port_number);//escuchamos el puerto indicado, ya sea el dinamico o el asignado localmente
+app.use(morgan('dev'));// utilizamos morgan como desarrollador
+app.use(express.json());//volvemos a instanciar express en caso de fallo al mb 
+app.use(express.urlencoded({extended:false}));//utilizamos el url encoded si es que en algun caso hayan datos delicados
 
-app.use(router)
+app.use(router)//ocupamos nuestro router 
 
-app.keepAliveTimeout = 30 * 1000;
+app.keepAliveTimeout = 30 * 1000;//tecnica de keepalive
 app.headersTimeout = 35 * 1000;
